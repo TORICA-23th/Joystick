cd9091ef698d1fa02102881b019e09e6d5969394
SeikaYoruno <misuzu14pc@gmail.com>
2023-06-06 21:15:15 +0900

    Update configServo.ino

--- configServo/configServo.ino
+++ configServo/configServo.ino
@@ -4,14 +4,23 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial1.begin(115200);
+  while(!Serial){}
   pinMode(3, OUTPUT);
   digitalWrite(3, HIGH);
   uint8_t TXdata[2] = { 0b10100000, 0x00};
-  
-  Serial.write(TXdata, 2);
+
+  Serial1.write(TXdata, 2);
+  Serial1.flush();
 
   digitalWrite(3, LOW);
 
+  while (true) {
+    while (Serial1.available() > 0) {
+      Serial.println(Serial1.read());
+    }
+  }
+  
 }
 
 void loop() {
